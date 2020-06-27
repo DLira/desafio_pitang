5ff25f32a9b052bc0d5181b7ef1da85e1dc1913b
Decio Lira <dl@deciolira.net>
2020-06-27 10:19:20 -0300

    Incluso funcoa para retornar o CPF com mascara.

--- questao3_queries.sql
+++ questao3_queries.sql
@@ -7,10 +7,24 @@
 --1. Retornar todos os usuários do sistema com o CPF (com máscara), o nome do us
 --uário em caixa alta, bem como os cargos, orgãos e sistemas associados (se existir);
 
-SELECT funcionario.funcionario_cpf AS CPF, UPPER(funcionario.nome) AS USUARIO, sistema.sistema_nome AS SISTEMA
+DELIMITER $$
+
+CREATE FUNCTION formatar_cpf(cpf VARCHAR(11))
+RETURNS VARCHAR(14)
+    BEGIN
+        RETURN CONCAT( SUBSTRING(cpf,1,3) , '.',
+                      SUBSTRING(cpf,4,3), '.',
+                      SUBSTRING(cpf,7,3), '-',
+                      SUBSTRING(cpf,10,2));
+	END$$
+DELIMITER ;
+
+
+SELECT formatar_cpf(funcionario.funcionario_cpf) AS CPF, UPPER(funcionario.nome) AS USUARIO, sistema.sistema_nome AS SISTEMA
 FROM funcionario 
 JOIN usuario_sistema
 ON funcionario.funcionario_cpf = usuario_sistema.funcionario_cpf
 JOIN sistema
 ON usuario_sistema.sistema_id = sistema.sistema_id
 ORDER BY funcionario.funcionario_cpf;
+
